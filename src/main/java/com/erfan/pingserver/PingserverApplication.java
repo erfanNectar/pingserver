f204f79f3edd777382421ca2e82de9e03668045e
erfanNectar <149995778+erfanNectar@users.noreply.github.com>
2023-11-07 10:20:36 +0530

    firstcommit

---
+++ src/main/java/com/erfan/pingserver/PingserverApplication.java
@@ -0,0 +1,13 @@
+package com.erfan.pingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erfan/pingserver/PingserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erfan.pingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
